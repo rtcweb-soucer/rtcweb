bfeee5f1f9c30c65de63212a0f731885e0a3e3e3
Welington <welington.lourenco.soares@gmail.com>
2026-02-13 17:37:08 -0300

    1737

--- pages/Quotes.tsx
+++ pages/Quotes.tsx
@@ -459,7 +459,7 @@ const Quotes = ({ orders, customers, technicalSheets, products, sellers, onUpdat
               <p className="text-[8px] font-black text-blue-600 uppercase tracking-widest mb-0.5">Contratada</p>
               <p className="text-xs font-black text-slate-900">RTC TOLDOS E COBERTURAS LTDA</p>
               <p className="text-[9px] text-slate-500 font-medium">CNPJ: 12.655.737/0001-21</p>
-              <p className="text-[9px] text-slate-500 font-medium">(21) 2281-8224 | (21) 99798-6419</p>
+              <p className="text-[9px] text-slate-500 font-medium">(21) 4062-7090 | (21) 2201-8118</p>
             </div>
           </div>
 
