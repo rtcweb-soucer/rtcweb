a6b3e0530f1aa3738aa6ae12f8918eaa641d28b5
Welington <welington.lourenco.soares@gmail.com>
2026-06-05 17:09:32 -0300

    1709

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.ie6jsd04r3g"
+    "revision": "0.lm0i3bmm008"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
