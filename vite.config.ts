47b7a16d93dfe16ed3e0e0532282039a48074ab0
Welington <welington.lourenco.soares@gmail.com>
2026-06-25 14:04:21 -0300

    chore: Increase chunk size warning limit

--- vite.config.ts
+++ vite.config.ts
@@ -85,6 +85,9 @@ export default defineConfig(({ mode }) => {
       alias: {
         '@': path.resolve(__dirname, '.'),
       }
+    },
+    build: {
+      chunkSizeWarningLimit: 2000,
     }
   };
 });
